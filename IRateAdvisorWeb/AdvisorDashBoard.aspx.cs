7c6594c7b39650799ae84062dede82babba825c1
Sanveer-Singh <sanveer001@gmail.com>
2020-09-27 01:19:00 +0200

    Added String Formatter That works

---
+++ IRateAdvisorWeb/AdvisorDashBoard.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IRateAdvisorWeb.Utilities;
+
+namespace IRateAdvisorWeb
+{
+    public partial class AdvisorDashBoard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Display.InnerHtml  = HtmlStringGenerator.GetDynamicReportDisplayString("This is a name", "https://wallpapercave.com/wp/wp2446333.jpg", "Field desc1","Field1","Field desc2", "Field2", "Field desc3", "Field3");
+
+        }
+    }
+}
--- IRateAdvisorWeb/Login.aspx.cs
+++ IRateAdvisorWeb/Login.aspx.cs
@@ -16,7 +16,35 @@ namespace IRateAdvisorWeb
 
         protected void btnLoginSubmit_Click(object sender, EventArgs e)
         {
+            // steps :
+            // get input from the form 
 
+            // sanatize input (Optional)
+
+            // test via client - get results
+
+            // set up session 
+
+            // edit highlighted navbar links 
+
+            //==== text box
+            string username = LoginUserName.Text;
+            // ===label of type input
+            string password = Password1.Value;
+           
+            //IRateAdvisorWeb.Client client = new IRateAdvisorWeb.Client();
+            // var result = await client.LoginUserAsync(Username,Pass);
+            // user my user = (IRateAdvisorWeb.Client.User )result
+            // if (result is null)
+            //{
+            //
+            //  SetError1("your username or password is incorrect")
+            //}
+            // else 
+            //{
+            // so
+            //Session["ID"] = result.UserId;
+            //}
         }
     }
 }
