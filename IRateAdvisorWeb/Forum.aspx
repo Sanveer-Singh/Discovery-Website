84775643965b2f087a47dc126810f01c3c20e5d3
JeanKempachi <jeankempachiboguo@gmail.com>
2020-09-27 02:29:32 +0200

    Added Forums page

---
+++ IRateAdvisorWeb/Forum.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Forum.aspx.cs" Inherits="IRateAdvisorWeb.Forum" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:TextBox ID="searchBox" runat="server" class="sm-form-control" Text="Search..."></asp:TextBox>
+    <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click"/>
+
+    <div id="displayDiv" runat="server" >
+
+    </div>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Footer1" runat="server">
+</asp:Content>
---
+++ IRateAdvisorWeb/Forum.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IRateAdvisorWeb.Utilities;
+
+namespace IRateAdvisorWeb
+{
+    public partial class Forum : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //TODO : Service call here to get object
+            string display = "";
+            string imgURL = "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/012020/thumbnail-discovery_logo_-_full_colour.png?o6Grbgwj5kb6vpMCXGaY_A5aeNuJTERJ&itok=mcA6_zSp";
+            
+            display += HtmlStringGenerator.GetDynamicReportDisplayString("This is test", imgURL, "This is description", "Field1", "This is description 2", "Field2", "Description 3", "Field 3");
+
+
+            displayDiv.InnerHtml = display;
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
